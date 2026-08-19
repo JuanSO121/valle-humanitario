fb952f917547d72416b7c58d27bce6ff8c575fb8
JuanSO121 <sanchezjuanjo1105@gmail.com>
2026-08-19 09:55:21 -0500

    link

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D5HQ21wx.mjs
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D5HQ21wx.mjs
@@ -1,20 +1,20 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BFCFVOQn.js
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-D5HQ21wx.js
 var tsrStartManifest = () => ({ routes: {
 	__root__: {
 		filePath: "C:/Users/JJSO/Documents/Sec_General/HAROLD/VALLE-GEO/vallegeo-explorer/src/routes/__root.tsx",
 		children: ["/"],
-		preloads: ["/assets/index-CE-RfDyQ.js"],
+		preloads: ["/assets/index-Bu5XIxGh.js"],
 		scripts: [{ attrs: {
 			type: "module",
 			async: !0,
-			src: "/assets/index-CE-RfDyQ.js"
+			src: "/assets/index-Bu5XIxGh.js"
 		} }]
 	},
 	"/": {
 		filePath: "C:/Users/JJSO/Documents/Sec_General/HAROLD/VALLE-GEO/vallegeo-explorer/src/routes/index.tsx",
 		children: void 0,
 		css: ["/assets/routes-CKRTiAqP.css"],
-		preloads: ["/assets/routes-qV0_czx3.js"]
+		preloads: ["/assets/routes-w8p6Wgqm.js"]
 	}
 } });
 //#endregion
